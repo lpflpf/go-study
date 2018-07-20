feecb837ee27eb23e2a749d35dd82f54cbd4dbc1
lipengfei <634626165@qq.com>
2018-07-20 20:27:04 +0800

    ch9

---
+++ src/dang/ch9/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "dang/ch9/memo1"
+import "fmt"
+import "time"
+import "log"
+import "net/http"
+import "io/ioutil"
+
+func httpGetBody(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+func main() {
+	m := memo1.New(httpGetBody)
+	for url := range incomingURLs() {
+		start := time.Now()
+		value, err := m.Get(url)
+		if err != nil {
+			log.Print(err)
+		}
+
+		fmt.Printf("%s, %s, %d bytes\n", url, time.Since(start), len(value.([]byte)))
+	}
+}
+func incomingURLs() <-chan string {
+	ch := make(chan string)
+	go func() {
+		for _, url := range []string{
+			"https://golang.org",
+			"https://godoc.org",
+			"https://play.golang.org",
+			"http://gopl.io",
+			"https://golang.org",
+			"https://godoc.org",
+			"https://play.golang.org",
+			"http://gopl.io",
+		} {
+			ch <- url
+		}
+		close(ch)
+	}()
+	return ch
+}
