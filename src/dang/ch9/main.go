4db9ce5e660a36a8cba6a22eed0887c518627a1d
lipengfei <634626165@qq.com>
2018-07-21 17:10:15 +0800

    Revert "Revert "update""
    This reverts commit da11ba3d0054847e8afbabf92a6cd236d33dfdd4.

--- src/dang/ch9/main.go
+++ src/dang/ch9/main.go
@@ -1,11 +1,12 @@
 package main
 
-import "dang/ch9/memo1"
+import memo "dang/ch9/memo5"
 import "fmt"
 import "time"
 import "log"
 import "net/http"
 import "io/ioutil"
+import "sync"
 
 func httpGetBody(url string) (interface{}, error) {
 	resp, err := http.Get(url)
@@ -18,16 +19,24 @@ func httpGetBody(url string) (interface{}, error) {
 }
 
 func main() {
-	m := memo1.New(httpGetBody)
+	m := memo.New(httpGetBody)
+	var n sync.WaitGroup
 	for url := range incomingURLs() {
-		start := time.Now()
-		value, err := m.Get(url)
-		if err != nil {
-			log.Print(err)
-		}
-
-		fmt.Printf("%s, %s, %d bytes\n", url, time.Since(start), len(value.([]byte)))
+		n.Add(1)
+		go func(url string) {
+			fmt.Println(url)
+			defer n.Done()
+			start := time.Now()
+			value, err := m.Get(url)
+			if err != nil {
+				log.Print(err)
+				return
+			}
+			fmt.Printf("%s, %s, %d bytes\n", url, time.Since(start), len(value.([]byte)))
+		}(url)
 	}
+
+	n.Wait()
 }
 func incomingURLs() <-chan string {
 	ch := make(chan string)
